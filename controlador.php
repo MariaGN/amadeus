d8cc28af598925472330a5d2fa8e61294214a899
MariaGN <>
2013-02-06 21:39:07 +0100

    añadido registro 6/02/13

---
+++ controlador.php
@@ -0,0 +1,18 @@
+<?php
+
+//Comprobamos si existe el fichero a cargar
+if (isset($_GET['inc'])&& $_GET['inc']!= "")
+    //comprobas si existe el fichero fisico a cargar
+    if (!file_exists("{$_GET['inc']}.php")) //vai entre {x ser 1 array}
+    {//sacamos página de error personalizada
+        header("HTTP/1.0 404 Not Found");
+        require 'error/404.html';
+    }
+    else
+    {
+        require_once 'header.html';
+        require_once 'menu.php';
+        require_once "{$_GET['inc']}.php";
+        require_once 'footer.html';
+    }
+?>
