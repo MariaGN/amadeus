b2fae250d1f9bf866962c44039d227b84f6f1229
MariaGN <>
2013-02-18 21:55:52 +0100

    Continuamos con ficheiro basedatos.php 	Crearfuncions: 				creamos constructor (vacio de momento) 				comprobar instancia 				cerrar conexion 				insertar Usuario Cambiamos nome variables en config.php
    Lunes 18/02/2013

--- lib/basedatos.php
+++ lib/basedatos.php
@@ -9,4 +9,93 @@
 //librerias necesarias
 require_once dirname(__FILE__).'/config.php';
 require_once dirname(__FILE__).'/funciones.php';
+
+class Basedatos{
+    /**
+     *
+     * @var Basedatos Contiene la instancia de la bbdd 
+     */
+    private static $_instancia;  //objeto de la abbdd  .servinos para indicar se está ou non creada a instancia
+    
+    /**
+     *
+     * @var boolean|mysqli Contiene el objeto mysqli despues de que se haya establecido la conexion 
+     */
+    private static $_mysqli=false;
+    
+    
+    private function __construct(){
+        
+    }
+    
+    /**
+     * Crea la conexión al servidor o devuelve error parando la ejecución
+     * 
+     * @return  Basedatos  Devuelve la referencia al objeto Basedatos
+     */
+    
+    public static function getInstancia(){
+        if(! self::$_instancia instanceof self){ //ou ben  Basedatos::$_instancia  //se nn é un objeto de unha instancia de unha clase (clase Basedatos)
+           
+            ////////////////// singleton
+            //Creamos una nueva instancia de bbdd
+            self::$_instancia= new self;  // ou ben   new Basedatos();
+            
+            //Creamos el  objeto mysqli y lo asignamos a $_mysqli
+            self::$_mysqli=@new mysqli(Config::$dbServidor,Config::$dbUsuario,Config::$dbPassword,Config::$dbDatabase);  //datos presentes en config.php, cuia clase é Config  e a cal chamamos o principio //@ é para non mostrar os erros
+            if (self::$_mysqli->connect_error){
+                echo "Error conectando Base Datos".self::$_mysqli->connect_error;  //sacamos o erro da conexion
+                self::$_mysqli=false;  //para indicar que non se pudo crear esa conexion
+                die();
+            }
+          /////////////////////// 
+            
+            //Si la instancia ya estaba creada la devolvemos
+            return self::$_instancia;   
+         }   
+    }
+  
+    
+    
+    
+         /**
+          * Cierra una conexion activa con el servidor
+          * 
+          * @access public
+          * @return boolean   Siempre devuelve  true(verdadero)
+          */
+     public function close(){
+         if(self::$_mysqli){
+             self::$_mysqli->close();
+             self::$_mysqli=false;
+         }
+         return true;
+     }
+    
+    
+         
+        
+         
+         
+         
+         public function insertarUsuario($nick,$password,$nombre,$apellidos,$dni,$email,$telefono){
+             //Preparamos la instruccion SQL.
+             $stmt=self::$_mysqli->prepare("insert into amadeus_usuarios (nick,password,nombre,apellidos,dni,email,telefono) values(?,?,?,?,?,?,?)") or die (self::$_mysqli->error);
+             //objeto tipo stement para
+             
+             //Enlazamos los parametros
+             $stmt->bind_param('sssssss',$nick,encriptar($password,10),$nombre,$apellidos,$dni,$email,$telefono);  //a pass a gardamos encriptada=> chamamos funcion encriptar de funciones.php
+             
+             
+             //Ejecutamos la instruccion
+             $stmt->execute() or die(self::$_mysqli->error);
+             
+             return "OK";
+         }
+    
+         
+         
+         
+    
+}
 ?>
--- lib/config.php
+++ lib/config.php
@@ -2,10 +2,10 @@
 //Datos de configuración de la aplicación
 class Config{
     //Configuracion de la bbdd
-    public static $db_servidor='mariagn.local';
-    public static $db_usuario="c29a11mariagn";
-    public static $db_password="abc123.";
-    public static $db_database="c29prueba";
+    public static $dbServidor='mariagn.local';
+    public static $dbUsuario="c29a11mariagn";
+    public static $dbPassword="abc123.";
+    public static $dbDatabase="c29prueba";
 
 }
 ?>
